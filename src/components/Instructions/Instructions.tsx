bf35567ead8ac61ef5137843d8c4cebb0c6870d6
crunchbar <>
2019-12-12 11:03:04 -0500

    Instructions update

--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -3,7 +3,6 @@ import {
   Collapse,
   Divider,
   IconButton,
-  Link,
   Paper,
   Typography,
 } from '@material-ui/core';
@@ -27,34 +26,33 @@ const Instructions: React.FC<InstructionsProps> = () => {
         >
           <ExpandMoreIcon />
         </IconButton>
+        <small>(Expand for full instructions)</small>
       </Typography>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <Divider />
         <Typography className="pad-1" component="div">
           You can change all of the function buttons on the QuickStrip (and the MORE Panel) as you prefer.
           <ol>
-            <li>Look through the list of features in the "All Choices List" - and check all that you might be interested in.
+            <li>Look through the list of features in the "All Choices List” at the bottom of the page - and check all that you might be interested in.
               <ul>
                 <li>When you check items in the "All Choices List" they will show up within "My Choices" so they are handy for exploring and putting into your QuickStrip or More Panel.</li>
+                <li>(There is an option on the “all Choices list” at the top that hides all those buttons you already have above - so that only new ones you don’t already have will appear in the “All Choices list”.)</li>
               </ul>
             </li>
             <li>You can then drag buttons back and forth between My Choices and the QuickStrip (or the MORE Panel) till you get what you want there.
               <ul>
-                <li>The strip will automatically make room and resize for any number of buttons you want to add.</li>
+                <li>The QuickStrip will automatically make room and resize for any number of buttons you want to add.</li>
+                <li>The items you have in your “My Choices” list will stay there if you want to leave some there for convenience if you do not want them in your QuickStrip or MORE panel right now.  Or you can return them to the “All Choices List”.</li>
               </ul>
             </li>
-            <li>You can also resize the QuickStrip yourself using the + and - buttons.</li>
-            <li>When you are done - click SAVE.</li>
+            <li>The strip will resize automatically if you need it to to accommodate a large number of buttons.   Or you can resize the QuickStrip yourself using the + and - buttons.</li>
+            <li>When you are done - click SAVE.
+              <ul>
+                <li>The next time you sign in or apply your preferences you will see your new QuickStrip.  If you want to see it right now - click on the “My Saved Settings” button and reapply your settings now.</li>
+              </ul>
+            </li>
           </ol>
-          NOTE: Everything can also be done by those who cannot "drag and drop" by using the left- and right-click buttons on the mouse/touch screen. Everything can also be done using the keyboard only, by using arrow key, tab, spacebar and return keys.
-          &nbsp;
-          <Link
-            href="https://morphic.world/"
-            target="_blank"
-            rel="noopener"
-          >
-            Learn more...
-          </Link>
+          Accessibility Note: Everything that can be done by "drag and drop” can also be done by 1) using the left- and right-click buttons on the mouse/touch screen,  OR  2) by using the keyboard only, by using arrow key, tab, spacebar and return keys.
         </Typography>
       </Collapse>
     </Paper>
