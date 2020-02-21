5fac13360e020b792ef4bcd36ad616f23c9ad661
crunchbar <>
2020-02-21 11:53:30 -0500

    v2.6.0

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -34,7 +34,7 @@ const App: React.FC = () => {
         <Container maxWidth={false}>
           <header className="app-header">
             <Logo />
-            v2.5.2
+            v2.6.0
           </header>
           <Instructions />
           <DragDropContainer {...dragDropContainerProps} />
--- src/components/Instructions/Instructions.tsx
+++ src/components/Instructions/Instructions.tsx
@@ -36,28 +36,46 @@ const Instructions: React.FC<InstructionsProps> = () => {
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <Divider />
         <Typography className="pad-1" component="div">
-          You can change all of the function buttons on the QuickStrip (and the MORE Panel) as you prefer.
+          <strong>How to change the buttons on the QuickStrip (and the MORE Panel)</strong>
           <ol>
-            <li>Look through the list of features in the "All Choices List” at the bottom of the page - and check all that you might be interested in.
+            <li>Put buttons you think you want to use into My Choices
               <ul>
-                <li>When you check items in the "All Choices List" they will show up within "My Choices" so they are handy for exploring and putting into your QuickStrip or More Panel.</li>
-                <li>(There is an option on the “all Choices list” at the top that hides all those buttons you already have above - so that only new ones you don’t already have will appear in the “All Choices list”.)</li>
+                <li>Add an existing button
+                  <ul>
+                    <li>Look through the list of buttons in the Morphic Button Catalog at the bottom of the page</li>
+                    <li>For buttons you want to use, click “Add to My Choices” next to those buttons</li>
+                  </ul>
+                </li>
+                <li>Make a new button
+                  <ul>
+                    <li>You can also make your own buttons by using one of the three options in the Make Your Own Button panel.</li>
+                  </ul>
+                </li>
               </ul>
             </li>
-            <li>You can then drag buttons back and forth between My Choices and the QuickStrip (or the MORE Panel) till you get what you want there.
+            <li>From My Choices, drag buttons into the QuickStrip or the More Panel.
               <ul>
-                <li>The QuickStrip will automatically make room and resize for any number of buttons you want to add.</li>
-                <li>The items you have in your “My Choices” list will stay there if you want to leave some there for convenience if you do not want them in your QuickStrip or MORE panel right now.  Or you can return them to the “All Choices List”.</li>
+                <li>The buttons in My Choices are buttons you made or pulled from the Catalog but haven’t added to the QuickStrip or More Panel
+                  <ul>
+                    <li>Any buttons you decide not to use you can to leave there for convenience</li>
+                    <li>Or you can return them to the Morphic Button Catalog.</li>
+                  </ul>
+                </li>
+                <li>The QuickStrip will resize automatically as you add buttons.</li>
               </ul>
             </li>
-            <li>The strip will resize automatically if you need it to to accommodate a large number of buttons.   Or you can resize the QuickStrip yourself using the + and - buttons.</li>
-            <li>When you are done - click SAVE.
+            <li>When you are done - click SAVE & EXIT button (don’t confuse this with the Save button in the QuickStrip!)
               <ul>
-                <li>The next time you sign in or apply your preferences you will see your new QuickStrip.  If you want to see it right now - click on the “My Saved Settings” button and reapply your settings now.</li>
+                <li>The next time you sign in or apply your preferences you will see your new QuickStrip.</li>
               </ul>
             </li>
           </ol>
-          Accessibility Note: Everything that can be done by "drag and drop” can also be done by 1) using the left- and right-click buttons on the mouse/touch screen,  OR  2) by using the keyboard only, by using arrow key, tab, spacebar and return keys.
+          Accessibility Note: Everything that can be done by "drag and drop” can also be done by:
+          <ul>
+            <li>Using the left- and right-click buttons on the mouse/touch screen</li>
+            OR
+            <li>Using the keyboard only, by using arrow, tab, spacebar, and return keys.</li>
+          </ul>
         </Typography>
       </Collapse>
     </Paper>
